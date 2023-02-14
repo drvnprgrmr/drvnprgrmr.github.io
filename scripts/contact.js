b85e8fde82c690b36f05bf4149ea84aecb7031d2
drvnprgrmr <ekunweibukun159357@gmail.com>
2023-02-14 16:02:03 +0100

    Fixed bug

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,15 +1,27 @@
 const form = document.getElementById("form")
 
+const nameEl = document.getElementById("name")
+const emailEl = document.getElementById("email")
+const subjectEl = document.getElementById("subject")
+const messageEl = document.getElementById("message")
+
 form.addEventListener("submit", async (e) => {
     e.preventDefault()
 
-    // Get form data from page
-    const mail = new FormData(form)
+    let mail = {
+        name: nameEl.value,
+        email: emailEl.value,
+        subject: subjectEl.value,
+        message: messageEl.value
+    }
 
     // Send mail
     const res = await fetch("https://portfolio-qbuq.onrender.com/contact", {
-        method: "post",
-        body: mail,
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(mail),
 
     }).then(response => {
         return response.json()
